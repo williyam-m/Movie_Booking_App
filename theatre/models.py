d7cbc3678ced7f80f72501fce3572dfebe0a9a9f
williyam-m <williyam64@gmail.com>
2024-09-02 20:11:21 +0530

    modify theatre model

--- theatre/models.py
+++ theatre/models.py
@@ -5,12 +5,3 @@ class Theatre(models.Model):
     id = models.UUIDField(primary_key = True, default = uuid.uuid4)
     name = models.CharField(max_length=255)
     location = models.CharField(max_length=255)
-
-'''
-class ShowTime(models.Model):
-    id = models.UUIDField(primary_key = True, default = uuid.uuid4)
-    screen_id = models.CharField(max_length=255)
-    theatre_id = models.CharField(max_length=255)
-    start_date = models.DateTimeField()
-    end_date = models.DateTimeField()
-'''
