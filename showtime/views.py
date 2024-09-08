cc35e10c6ec4b9157bf80fddc780e8e0a3ee02c4
williyam-m <williyam64@gmail.com>
2024-09-08 17:06:08 +0530

    feat : add theatre alteration when edit showtime

--- showtime/views.py
+++ showtime/views.py
@@ -54,10 +54,12 @@ def edit_showtime(request, pk):
     showtime = get_object_or_404(ShowTime, pk=pk)
     if request.method == 'POST':
         movie_id = request.POST.get('movie')
+        theatre_id = request.POST.get('theatre')
         screen_id = request.POST.get('screen')
         start_date = request.POST.get('start_date')
 
         movie = get_object_or_404(Movie, pk=movie_id)
+        theatre = get_object_or_404(Theatre, pk=theatre_id)
         screen = get_object_or_404(Screen, pk=screen_id)
         end_date = datetime.fromisoformat(start_date) + timedelta(minutes=movie.duration)
 
@@ -68,14 +70,15 @@ def edit_showtime(request, pk):
 
         showtime.movie = movie
         showtime.screen = screen
+        showtime.theatre = theatre
         showtime.start_date = datetime.fromisoformat(start_date)
         showtime.end_date = end_date
         showtime.save()
         return redirect('showtime_list')
 
     movies = Movie.objects.all()
-    screens = Screen.objects.all()
-    return render(request, 'edit_showtime.html', {'showtime': showtime, 'movies': movies, 'screens': screens})
+    theatres = Theatre.objects.all()
+    return render(request, 'edit_showtime.html', {'showtime': showtime, 'movies': movies, 'theatres': theatres})
 
 
 def delete_showtime(request, pk):
