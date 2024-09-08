30dc44cdefc0be0be5b11b8c1914c0218f5609ea
williyam-m <williyam64@gmail.com>
2024-09-08 15:04:50 +0530

    create dashboard views.py

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.db.models import Count
+from movie.models import Movie
+from theatre.models import Theatre
+from screen.models import Screen
+from showtime.models import ShowTime
+from user.models import User
 
-# Create your views here.
+def dashboard(request):
+    context = {
+        'movies_count': Movie.objects.count(),
+        'theatres_count': Theatre.objects.count(),
+        'screens_count': Screen.objects.count(),
+        'showtimes_count': ShowTime.objects.count(),
+        'users_count': User.objects.count(),
+    }
+    return render(request, 'main-dashboard.html', context)
