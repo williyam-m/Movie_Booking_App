3b2581c1cc7c67ce0518c0c4607e510de43aa686
williyam-m <williyam64@gmail.com>
2024-09-08 15:03:34 +0530

    alter urls.py

--- showtime/urls.py
+++ showtime/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('create/', views.create_showtime, name='create_showtime'),
-    path('', views.showtime_list, name='showtime_list'),
+    path('dashboard/', views.showtime_list, name='showtime_list'),
     path('<str:pk>/edit/', views.edit_showtime, name='edit_showtime'),
     path('<str:pk>/delete/', views.delete_showtime, name='delete_showtime'),
     path('<str:pk>/', views.view_showtime, name='view_showtime'),
