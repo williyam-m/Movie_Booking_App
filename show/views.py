3e529fdb9c87ef16bafc7d93f04bf764a985d33e
williyam-m <williyam64@gmail.com>
2024-09-12 16:47:21 +0530

    feat : filter show listing by date

--- show/views.py
+++ show/views.py
@@ -1,5 +1,6 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
+from django.utils.dateparse import parse_date
 from movie.models import Movie
 from showtime.models import ShowTime
 from theatre.models import Theatre
@@ -7,7 +8,15 @@ from theatre.models import Theatre
 
 def show_movie_theatres(request, movieid):
     movie = get_object_or_404(Movie, id=movieid)
-    showtimes = ShowTime.objects.filter(movie=movie).order_by('theatre', 'start_date')
+    filter_date = request.GET.get('filter_date')
+
+    if filter_date:
+        showtimes = ShowTime.objects.filter(movie=movie, start_date__date=parse_date(filter_date)).order_by('theatre',
+                                                                                                            'start_date')
+    else:
+        showtimes = ShowTime.objects.filter(movie=movie).order_by('theatre', 'start_date')
+
+    theatres = Theatre.objects.filter(showtime__in=showtimes).distinct()
 
     paginator = Paginator(showtimes, 10)  # Show 10 showtimes per page
     page_number = request.GET.get('page')
@@ -15,6 +24,8 @@ def show_movie_theatres(request, movieid):
 
     context = {
         'movie': movie,
+        'theatres': theatres,
         'page_obj': page_obj,
+        'filter_date': filter_date,
     }
     return render(request, 'show_movie_theatres.html', context)
