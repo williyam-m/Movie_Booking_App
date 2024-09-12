5d183cd814ec6cb2e623196bd5221811d94d078f
williyam-m <williyam64@gmail.com>
2024-09-12 12:53:26 +0530

    add show listing logic and template with paginator

--- show/urls.py
+++ show/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+path('<str:movieid>/', views.show_movie_theatres, name='show_movie_theatres'),
 ]
--- show/views.py
+++ show/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.core.paginator import Paginator
+from django.shortcuts import render, get_object_or_404
+from movie.models import Movie
+from showtime.models import ShowTime
+from theatre.models import Theatre
 
-# Create your views here.
+
+def show_movie_theatres(request, movieid):
+    movie = get_object_or_404(Movie, id=movieid)
+    showtimes = ShowTime.objects.filter(movie=movie).order_by('theatre', 'start_date')
+
+    paginator = Paginator(showtimes, 10)  # Show 10 showtimes per page
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    context = {
+        'movie': movie,
+        'page_obj': page_obj,
+    }
+    return render(request, 'show_movie_theatres.html', context)
