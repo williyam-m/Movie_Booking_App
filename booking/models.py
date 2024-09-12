951a6e77818573f2607f273a4b2b2ccdf2a44a85
williyam-m <williyam64@gmail.com>
2024-09-12 23:02:25 +0530

    create booking model

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+import uuid
+from django.contrib.auth.models import User
+from showtime.models import ShowTime
 
-# Create your models here.
+class Booking(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    showtime = models.ForeignKey(ShowTime, on_delete=models.CASCADE)
+    seats = models.JSONField(default=list)
+    amount_paid = models.DecimalField(max_digits=10, decimal_places=2)
+    booking_datetime = models.DateTimeField(auto_now_add=True)
+
